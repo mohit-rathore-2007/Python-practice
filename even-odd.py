2bc0f52f2237e1a9f6032cabb435fbc16974e14c
mohit-rathore-2007 <mohiraofficial2007@gmail.com>
2026-02-06 15:31:37 +0530

    update file

---
+++ even-odd.py
@@ -0,0 +1,15 @@
+
+x=int(input("enter value of x"))
+y=int(input("enter the value of y"))
+if x%2==0:
+    print("x is even")
+else:
+	print("x is odd")
+if x%4==0:
+	print=("x is completely divisible by 4")
+else:
+	print("x is not completely divisible by 4")
+if x%y==0:
+	print("x is completely divisible by y")
+else:
+	print("x is not completely divisible by y")
