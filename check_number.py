6459731bd694866b5cb98f937ff76dc6c7a3e9f4
mohit-rathore-2007 <mohiraofficial2007@gmail.com>
2026-02-19 16:55:03 +0530

    file added

---
+++ check_number.py
@@ -0,0 +1,8 @@
+#WPSto Check if the first and last number of a list is same or not
+
+l = [1, 6, 2, 52, 7, 12, 1, 15, 5]
+
+if l[0] == l[-1]:
+	print("first and last number is same")
+else:
+	print("They are not same")
