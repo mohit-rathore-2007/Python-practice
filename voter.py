af43b42c0380e79c6cf1d18c24dfa2ecdacec73f
mohit-rathore-2007 <mohiraofficial2007@gmail.com>
2026-02-19 16:32:21 +0530

    file added

---
+++ voter.py
@@ -0,0 +1,8 @@
+#1.WPS to check whether a person is eligible to vote or not based on their age
+
+age = float(input("Enter your age : "))
+
+if age >= 18:
+	print("You are eligible for voting")
+else:
+	print("You are not eligible for voting")
