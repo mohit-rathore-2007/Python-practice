85804847f9e12654a0dc0c2d3d138fd3e8367b29
mohit-rathore-2007 <mohiraofficial2007@gmail.com>
2026-02-26 17:48:21 +0530

    file added

---
+++ addition.py
@@ -0,0 +1,39 @@
+x = int(input("enter value of x"))
+y = int(input("enter value of y"))
+z = x+y
+a = x-y
+b = x*y
+c = x/y
+d = x%y
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
+print(c)
+print(d)
+print("x+y=",z)
+print("x-y=",a)
+print("x*Y=",b)
+print("x/y=",c)
+print("x% y=",d)
+print(x,"+",y,"=",z)
+print(x,"-",y,"=",a)
+print(x,"*",y,"=",b)
+print(x,"/",y,"=",c)
+print(x,"%",y,"=",d)
+print("x=",x)
+print("y=",y)
+print("x+y=",z)
+print("x-y=",a)
+'''x = int(input("enter value of x"))
+y = int(input("enter value of y"))
+print("value before swapping")
+print("x=",y)
+print("y",y)
+x=x+y
+y=x-y
+x=x-y
+print("value after swapping")
+print("x=",x)
+print("y=",y)'''
