710a8df125e3e98839baa9dcb3eb2821365815b7
mohit-rathore-2007 <mohiraofficial2007@gmail.com>
2026-02-27 17:56:06 +0530

    file added

---
+++ info_list.py
@@ -0,0 +1,7 @@
+#1 to store user given information (name,add,mail,con.no) in a list and display it
+items=input("Enter your Information :")
+info_a=[]
+print(info_a)
+
+info_a.append(items)
+print(info_a)
