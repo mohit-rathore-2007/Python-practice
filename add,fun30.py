21e6d1878edccec60979eb84197748043c39cd8a
mohit-rathore-2007 <mohiraofficial2007@gmail.com>
2026-04-02 16:40:13 +0530

    removed unnecessary files

--- add,fun30.py
+++
@@ -1,7 +0,0 @@
-#Que-1
-a=int(input("enter first value: "))
-b=int(input("enter second value: "))
-def add(a,b):
-	x=a+b
-	print(x)
-add(a,b)
--- area,fun30.py
+++
@@ -1,6 +0,0 @@
-#Que-2
-r=int(input("enter the radius"))
-def area(r):
-	area_of_circle=(22/7*r*r)
-	print("result", area_of_circle)
-area(r)
