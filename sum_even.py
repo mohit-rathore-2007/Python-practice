8f96c2551c34181c64fef3278579d95e1a835e6b
mohit-rathore-2007 <mohiraofficial2007@gmail.com>
2026-04-29 16:49:46 +0530

    file added

---
+++ sum_even.py
@@ -0,0 +1,10 @@
+#Que-15 sum of the even numbers
+
+def sum_of_even(n):
+	total = 0
+	for i in range (n+1):
+		if i % 2 == 0:
+			total += i
+	print("result",total)
+n=int(input("enter the values"))
+sum_of_even(n)
