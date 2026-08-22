c5d8c7c38db6fd42daa8a1e4fc039bd5a5d54dc6
Mohit Rathod <mohiraofficial2007@gmail.com>
2026-08-22 23:00:26 +0530

    file added

---
+++ list_ends.py
@@ -0,0 +1,17 @@
+'''Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) 
+and makes a new list of only the first and last elements of the given list.'''
+
+
+a = [5, 10, 15, 20, 25]
+
+result_lst = []
+
+start_end = a[0], a[-1]
+
+result_lst.append(start_end)
+
+print(result_lst)
+
+
+
+
