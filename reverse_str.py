e7664c56bb6a99a12c6b466271179a37b526b19e
Mohit Rathod <mohiraofficial2007@gmail.com>
2026-08-24 20:55:20 +0530

    file added

---
+++ reverse_str.py
@@ -0,0 +1,18 @@
+'''Write a program (using functions!) that asks the user for a long string containing multiple words.
+ Print back to the user the same string, except with the words in backwards order. For example, 
+ say I type the string: my name is  mohit 
+output = mohit name is my'''
+
+
+def reverse_string(sentence):
+
+	words = sentence.split()
+
+	rev_string = words[::-1]
+
+
+	return " ".join(rev_string)
+
+sentence = input("Enter a string ")
+
+print(reverse_string(sentence))  
