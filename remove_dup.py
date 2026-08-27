b82bc98b77c2b78add075739b6a7a7259b6f4113
Mohit Rathod <mohiraofficial2007@gmail.com>
2026-08-27 13:15:31 +0530

    file added

---
+++ remove_dup.py
@@ -0,0 +1,15 @@
+def remove_duplicate(x):
+
+
+	result_lst = []
+
+	for i in x:
+
+		if i not in result_lst:
+			result_lst.append(i)
+
+	return result_lst
+
+a = [1, 2, 2, 3, 4, 4, 5]
+print(remove_duplicate(a)) 
+
