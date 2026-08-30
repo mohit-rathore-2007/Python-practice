0f6135ac005b372f6b4c9f71c0bb9ae7bce64baf
Mohit Rathod <mohiraofficial2007@gmail.com>
2026-08-30 21:38:56 +0530

    file added

---
+++ find_num.py
@@ -0,0 +1,25 @@
+'''Practice Problem: Write a program to find the largest and smallest digit within a given integer 
+(e.g., in 75869, the largest is 9 and the smallest is 5'''
+
+
+num = 75869
+
+
+digits = []
+
+for d in str(num):
+	digits.append(int(d))
+
+largest = digits[0]
+smallest = digits[0]
+
+for i in digits:
+	if i > largest:
+		largest = i
+
+	if i < smallest:
+		smallest = i
+
+print("largest", largest)
+print("Smallest", smallest)
+	   	
