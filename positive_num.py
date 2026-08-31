b4b8b1540a0a0feae8eca3998e9c87891be49ed0
Mohit Rathod <mohiraofficial2007@gmail.com>
2026-08-31 17:57:20 +0530

    file added

---
+++ positive_num.py
@@ -0,0 +1,20 @@
+'''num = int(input("Enter a number : "))
+
+if num < 0:
+	print("The given number is Negative")
+elif num > 0:
+	print("The given number is Positive")
+
+else:
+	if num == 0:
+		print("The given number is ZERO")
+
+
+num = int(input("enter a number"))
+
+'''
+
+num = int(input("enter a num : "))
+
+for i in range(1,num):
+	print(i)
