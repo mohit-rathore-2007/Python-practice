8520988fa24991bf3296a4e2443edc446379e604
Mohit Rathod <mohiraofficial2007@gmail.com>
2026-09-06 17:41:25 +0530

    file added

---
+++ unique_list.py
@@ -0,0 +1,17 @@
+#Write a Python program to find the common elements between two lists
+
+def find_common_elements(list1, list2):
+	common_elements = []
+
+	for item in list1:
+		if item in list2:
+			common_elements.append(item)
+
+	return common_elements
+
+list1 = ["mohit", "rathod", "he", "is", "hello world"]
+
+list2 = ["mohit", "rathod", "he", "hello python"]
+
+result = find_common_elements(list1,list2)
+print(result)
