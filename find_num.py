629a7a00128da6f55f39a60b8c2122e6d8062b14
Mohit Rathod <mohiraofficial2007@gmail.com>
2026-09-11 20:09:17 +0530

    changes

--- find_num.py
+++ find_num.py
@@ -1,6 +1,5 @@
 '''Practice Problem: Write a program to find the largest and smallest digit within a given integer 
-(e.g., in 75869, the largest is 9 and the smallest is 5'''
-
+(e.g., in 75869, the largest is 9 and the smallest is 5. '''
 
 num = 75869
 
